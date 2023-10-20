c8233ba91bc179b32177cf5583734bfc94f900a3
LawsonJohnson <lawson-git@pm.me>
2023-10-20 11:51:07 -0400

    Prettier formatting

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-/*jshint esversion: 8 */
+Hooks.once("ready", () => {
+	if (!game.modules.get("lib-wrapper")?.active && game.user.isGM)
+		ui.notifications.error("Module Macro Icons requires the 'libWrapper' module. Please install and activate it.");
 
-Hooks.once('ready', () => {
-  if(!game.modules.get('lib-wrapper')?.active && game.user.isGM)
-      ui.notifications.error("Module Macro Icons requires the 'libWrapper' module. Please install and activate it.");
+	libWrapper.register(
+		"macro-icons",
+		"TextEditor._createContentLink",
+		function (wrapped, ...args) {
+			const [match, type, target, name] = args;
 
-  libWrapper.register('macro-icons', 'TextEditor._createContentLink', function (wrapped, ...args) {
-    const [ match, type, target, name ] = args;
-    
-    let result = wrapped(...args);
+			let result = wrapped(...args);
 
-    if(type === "Macro") {
-      let macro = game.macros.get(target);
+			if (type === "Macro") {
+				let macro = game.macros.get(target);
 
-      if(macro.data.img) {
-        result.classList.add('macro-icons-macro-icon-link');
-        result.innerHTML = "<img src='" + macro.data.img + "' class='macro-icons-macro-icon-img'> " + name;
-      }
-    }
+				if (macro.data.img) {
+					result.classList.add("macro-icons-macro-icon-link");
+					result.innerHTML = "<img src='" + macro.data.img + "' class='macro-icons-macro-icon-img'> " + name;
+				}
+			}
 
-    return result;
-  }, 'WRAPPER');
+			return result;
+		},
+		"WRAPPER"
+	);
 });
-
